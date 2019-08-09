72096c59fa9010d44594b08ad18386d46b642d8d
Nijil Narayanan <nijilap@yahoo.com>
2019-08-09 10:33:54 +0530

    css implementation

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import Logo from './../Assets/Logo.png'
 import './Header.css'
 
 const Header = () => (
-    <header className='header'></header>
+    <header className='header'>
+        <div className='Logo'>
+            <img src={Logo} alt='logo'/>
+        </div>
+        <div className='name-and-options'>
+            <div className='spreasheet-name'>Untiled Spreadsheet</div>
+            <div className='options'>
+                <span>File</span>
+                <span>Edit</span>
+                <span>View</span>
+                <span>Insert</span>
+                <span>Format</span>
+                <span>Data</span>
+                <span>Tools</span>  
+                <span>Add-ons</span>
+                <span>Help</span>
+            </div>
+        </div>
+    </header>
 )
 
 export default Header
